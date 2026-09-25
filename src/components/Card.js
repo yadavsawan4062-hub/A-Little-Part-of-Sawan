0dc2c249dbf8b61c69ddc076a7910eb710075ac9
Sawan Yadav <yadavsawan4062@gmail.com>
2026-09-25 15:47:09 +0530

    Fix image alt attribute

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,7 +12,7 @@ function Card({id, image, info, price,name,removeTour}){
  
     return(
         <div className="card">
-            <img src={image} className="image"></img>
+        <img src={image} className="image" alt={name} />
         <div className="tour-info">
             <div className="tour-details">
                 <h4 className="tour-price">₹ {price}</h4>
